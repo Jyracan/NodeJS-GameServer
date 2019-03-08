b364fc3f89db5b4ebcf8fb2b12d9b0acd252b02b
jyra <cmoineau@enssat.fr>
2019-03-08 14:39:38 +0100

    Utilisation d'ejs pour rediriger vers une page de choix de jeu Création d'un jeu de hasard

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express')
+const bodyParser=require('body-parser')
+//recquire body-parser regarder sur internet !
 const app = express()
 const port = 3000
 const path="/"
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //app.use(express.static('assets'))
 
 //Redirection to the login page
 app.get('/', function (req, res) {
-	res.sendFile(__dirname+'/assets/connexion.html')
+	res.render('connexion.ejs', {})
 	console.log('Affichage de la page de connexion')
 	//res.render('assets/connexion')
 })
-app.post('/index' , function (req, res) {
-	res.render('chambre.ejs', {etage : 1})
+app.post('/index' , function (req, res) { //utiliser req.body.login pour attrapper le login
+	res.render('accueil.ejs', {login : req.body.login})
 	console.log('Affichage de la page de choix de jeu')
 	//res.render('assets/connexion')
 })
+
+app.get('/devinette', function (req, res) {
+	res.sendFile(__dirname+'/jeux/devinette/devinette.html')
+	console.log('Lancement du jeux devinette')
+	//res.render('assets/connexion')
+}) 
 
 app.listen(port, () => console.log('http://localhost:'+port+path))
