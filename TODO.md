ecc72845e36e4cdb1451baf227943895bc0a0ec2
jyra <cmoineau@enssat.fr>
2019-03-24 09:24:41 +0100

    TODO : Comprendre pourquoi cookie-parser fait planter le site ...

--- TODO.md
+++ TODO.md
@@ -1,10 +1,11 @@
 # TODO list projet NodeJS :
  - [] Mettre le nom de l'utilisateur dans le cookie ...
  - [] Faire une redirection quand l'utilisateur arrive sur le site à une addresse du genre '/' ...
+ - [] Utiliser un cookie pour avoir le nom de lutilisateur
  - [] Faire des redirections si pas de cookie de session 
  - [X] Créer un dossier routes pour alléger l'index.js
  - [] Gérer la connexion à la base de donnée
  - [] Envoyer les informations à la base de donnée après une partie via un POST
  - [] réaliser de la doc (README ?)
- - 
+ - [] Regarder bootstrap
  
--- utils/sessionChecker.js
+++ utils/sessionChecker.js
@@ -1,5 +1,4 @@
 exports.login = function (req, res) {
-	//this.user_identifier=req.body.login
 	console.log('Identification de l\'utilisateur ')
 	res.cookie('session', 'True', { maxAge: 900000, httpOnly: true })
 	res.cookie('userName',req.body.login, { maxAge: 900000, httpOnly: true })
@@ -9,17 +8,14 @@ exports.logout = function (req, res) {
 	this.user_identifier=''
 	console.log('Suppression du cookie de session')
 	res.clearCookie('session')
+	res.clearCookie('userName')
 	res.redirect('/login')
 }
 exports.check = function (req, res) {
 	// TODO : Si le cookie de session est à false on redirige a '/'
 	console.log('###\nTest de routine du cookie de session ...');
+	//console.log('cookies' + req.cookies.session)
+	//flag=false
+	console.log('Fin\n###');
 
-	/*
-	for (cookie in cookies) {
-		if(cookie.toString()=='True'){
-			console.log('La vérifiaction de cookie fonctionne !')
-		}
-	}*/
-	//console.log('Cookie valide, utilisateur ' + this.user_identifier + ' reconnu\n###');
 }
